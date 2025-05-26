319d4202186b4535cb6722d5cba576f20569bf4f
Luciano Molaro <molarolucianodelongo@gmail.com>
2025-05-26 03:44:00 -0300

    agrego una conexion a la base de datos, differente de la del profe

--- src/main/java/BaseConnection/ConectorDB.java
+++ src/main/java/BaseConnection/ConectorDB.java
@@ -1,4 +1,16 @@
 package BaseConnection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConectorDB {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/BuenSaborDB";
+    private static final String USER = "user";
+    private static final String PASS = "";
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, "");
+    }
+
 }
